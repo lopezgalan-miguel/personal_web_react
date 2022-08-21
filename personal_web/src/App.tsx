8fa393100182cbe7208d7e19f4e1c813ac59b39c
lopezgalan-miguel <57156297+lopezgalan-miguel@users.noreply.github.com>
2022-08-21 20:38:21 +0200

    Clean default data
    Clean default data and set basic structure

--- personal_web/src/App.tsx
+++ personal_web/src/App.tsx
@@ -1,24 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="container">
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+         {/*  TODO Set Header */}
+         {/*  TODO Set body */}
+         {/*  TODO Set footer */}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
