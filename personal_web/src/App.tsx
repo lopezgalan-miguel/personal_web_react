028a468b7321704e77bebad3a8c88c39461c27e4
lopezgalan-miguel <57156297+lopezgalan-miguel@users.noreply.github.com>
2022-08-21 20:45:16 +0200

    set bootstrap

--- personal_web/src/App.tsx
+++ personal_web/src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="container">
+      <button type="button" className="btn btn-primary">Primary</button>
         <p>
          {/*  TODO Set Header */}
          {/*  TODO Set body */}
